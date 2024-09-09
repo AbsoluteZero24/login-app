770186ba25e95194473ed1beddc495843cc5f7c9
aditia <aditiaersa@gmail.com>
2024-09-09 09:21:12 +0700

    first

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+$host = 'mysql'; // ini adalah service name MySQL di Kubernetes
+$user = 'root';
+$password = 'rootpassword';
+$dbname = 'login_db';
+
+$conn = new mysqli($host, $user, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $_SESSION['loggedin'] = true;
+        $_SESSION['username'] = $username;
+        echo "Login berhasil! Selamat datang, " . $username;
+    } else {
+        echo "Login gagal. Username atau password salah.";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+    <h2>Form Login</h2>
+    <form method="POST" action="">
+        Username: <input type="text" name="username" required><br>
+        Password: <input type="password" name="password" required><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
